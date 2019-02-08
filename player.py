560ea0923461ac2262d3d61fe12a004107f2b698
Menesi Zoltan <colos.menesi@gmail.com>
2019-02-08 11:01:15 +0100

    1.st iter

--- player.py
+++ player.py
@@ -3,7 +3,20 @@ class Player:
     VERSION = "Gopnik_FM_ver_1.0"
 
     def betRequest(self, game_state):
-        return 0
+        our_bet = 0
+        our_cards = None
+
+        for players in game_state["players"]:
+            if players["name"] == "Gopnik FM":
+                our_cards = players["hole_cards"]
+
+        if our_cards[0]["rank"] == our_cards[1]["rank"] and our_cards[0]["rank"] in ["J", "K", "Q", "A"]:
+            if game_state["current_buy_in"] == 0:
+                our_bet = 100
+            else:
+                our_bet = game_state["current_buy_in"] * 2
+
+        return our_bet
 
     def showdown(self, game_state):
         pass
