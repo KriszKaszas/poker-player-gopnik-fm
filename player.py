66fa9b8450e5e2908e0d5e722b72c76010f1891c
Menesi Zoltan <colos.menesi@gmail.com>
2019-02-08 15:17:08 +0100

    added try catch

--- player.py
+++ player.py
@@ -6,62 +6,79 @@ class Player:
 
     def betRequest(self, game_state):
         print('-------------------------HELLLLLLOOOOO------------------------------')
+        our_bet = 0
+        values = {'2': 2,
+                  '3': 3,
+                  '4': 4,
+                  '5': 5,
+                  '6': 6,
+                  '7': 7,
+                  '8': 8,
+                  '9': 9,
+                  '10': 10,
+                  'J': 11,
+                  'Q': 12,
+                  'K': 13,
+                  'A': 14}
+
+
+        #-----------------------CARDS----------------------------
+        cards_on_table = []
         try:
-            our_bet = 0
-            values = {'2': 2,
-                      '3': 3,
-                      '4': 4,
-                      '5': 5,
-                      '6': 6,
-                      '7': 7,
-                      '8': 8,
-                      '9': 9,
-                      '10': 10,
-                      'J': 11,
-                      'Q': 12,
-                      'K': 13,
-                      'A': 14}
-
-
-            #-----------------------CARDS----------------------------
-            cards_on_table = []
             for card in game_state["community_cards"]:
                 cards_on_table.append(card["rank"])
+        except Exception:
+            print('-----------------------------ERROR 1---------------------------')
 
 
-            stack = 0
-            our_cards = None
+        stack = 0
+        our_cards = None
+        try:
             for players in game_state["players"]:
                 if players["name"] == "Gopnik FM":
                     our_cards = players["hole_cards"]
                     stack = players["stack"]
+        except Exception:
+            print('-----------------------------ERROR 2---------------------------')
 
 
+        collection = our_cards + cards_on_table
 
-            collection = our_cards + cards_on_table
+        try:
             collection.sort(key=lambda x: values[x])
-            #---------------------------------------------------------
+        except Exception:
+            print('-----------------------------ERROR 3---------------------------')
+        #---------------------------------------------------------
 
 
-            cards = {}
+        cards = {}
+        try:
             for card in collection:
                 if card not in cards:
                     cards[card] = 1
                 else:
                     cards[card] += 1
+        except Exception:
+            print('-----------------------------ERROR 4---------------------------')
 
 
+        try:
             for rank,number in cards.items():
                 if number == 2 and values[rank] >= 10:
                     our_bet += game_state["current_buy_in"] * 1.25
+        except Exception:
+            print('-----------------------------ERROR 5---------------------------')
 
 
+        try:
             for rank,number in cards.items():
                 if number == 3 and values[rank] >= 5:
                     our_bet = game_state["current_buy_in"] * 1.5
+        except Exception:
+            print('-----------------------------ERROR 6---------------------------')
 
-
-            card_cnt = 0
+        card_cnt = 0
+        try:
             for i in range(len(collection) - 1):
                 if values[collection[i + 1]] - values[collection[i]] > 1:
                     card_cnt = 0
@@ -71,14 +88,14 @@ class Player:
                 if card_cnt == 4:
                     our_bet = game_state["current_buy_in"] * 1.75
                     break
-
-
-            if our_bet > stack:
-                our_bet = stack
-
-            return our_bet
         except Exception:
-            print("Error somewhere")
+            print('-----------------------------ERROR 7---------------------------')
+
+        if our_bet > stack:
+            our_bet = stack
+
+        return our_bet
+
 
     def showdown(self, game_state):
         pass
